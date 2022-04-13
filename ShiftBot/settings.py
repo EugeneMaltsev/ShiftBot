9f00bd3408c034caa21811529b7e9a5def8cf882
Eugene Maltsev <67748456+EugeneMaltsev@users.noreply.github.com>
2022-04-13 11:44:00 +0300

    Setting up the project
    The PostgreSQL was connected.
    The shift application has been connected and configured.
    Migration applied.

--- ShiftBot/settings.py
+++ ShiftBot/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'shift.apps.ShiftConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'ShiftBot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ShiftBot',
+        'USER': 'postgres',
+        'PASSWORD': 'cheshuya123',
+        'HOST': 'localhost'
     }
 }
 
